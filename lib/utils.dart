15d068d288c085d45cfef7d3e49f919eea988b1a
wangkun <wangkun@woodemi.com>
2021-02-27 20:27:53 +0800

    WIP: Impl on Windows
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- lib/utils.dart
+++ lib/utils.dart
@@ -10,6 +10,8 @@ Iterable<int> iterateInt8(Pointer<Int8> ptr) sync* {
 }
 
 /// macOS: [The wchar_t type is 32 bit and is a signed type.](https://developer.apple.com/documentation/xcode/writing_arm64_code_for_apple_platforms)
+/// 
+/// Windows: [In the Microsoft compiler, it represents a 16-bit wide character used to store Unicode encoded as UTF-16LE, the native character type on Windows operating systems.](https://docs.microsoft.com/en-us/cpp/cpp/char-wchar-t-char16-t-char32-t)
 String fromWChar(Pointer<Int32> wstring) => utf8.decode(iterateInt32(wstring).toList());
 
 Iterable<int> iterateInt32(Pointer<Int32> ptr) sync* {
